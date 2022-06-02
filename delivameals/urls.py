ab7d4eda696e970ef6605341d2bd831f73ebc9e2
sylva <sylvaejike@gmail.com>
2022-06-02 12:41:28 +0100

    'update'

--- delivameals/urls.py
+++ delivameals/urls.py
@@ -38,10 +38,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('core.urls')),
-    path('auth/', include('authentication.urls')),
+    path('api/', include('core.urls')),
+    path('api/auth/', include('authentication.urls')),
     path('playground/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
